24d80f0c36750b8435cb0ef661a983ad235d92e5
Nate <choseongho93@naver.com>
2021-04-04 00:30:11 +0900

    provider repo add2

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Repositories\Log\DelayRepo;
+use App\Repositories\Log\IDelayRepo;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
